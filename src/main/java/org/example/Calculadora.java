a391572789aa84485b10d6d50aa45b2717258536
Pamela Morsentti <pamelamorsentti@gmail.com>
2023-06-17 19:29:11 -0300

    correccion metodo dividir

--- src/main/java/org/example/Calculadora.java
+++ src/main/java/org/example/Calculadora.java
@@ -1,8 +1,35 @@
 package org.example;
-
 public class Calculadora {
+    private int numero1;
+    private int numero2;
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+// --Commented out by Inspection START (17-Jun-23 19:04):
+//    private void Calculadora(int x, int y) {
+//        this.numero1 = x;
+//        this.numero2 = y;
+//    }
+// --Commented out by Inspection STOP (17-Jun-23 19:04)
+
+    public int sumar(int x, int y) {
+        return x + y;
+    }
+
+    public int restar(int x, int y) {
+        return x - y;
+    }
+
+    public int multiplicar(int x, int y) {
+        return x * y;
+    }
+    public double dividir(int x, int y) {
+        return (double) x / y; }
+
+    public int getNumero1() {
+        return numero1;
+    }
+
+    public void setNumero1(int numero1) {
+        this.numero1 = numero1;
     }
 }
+
--- src/test/java/org/example/CalculadoraTest.java
+++ src/test/java/org/example/CalculadoraTest.java
@@ -1,4 +1,54 @@
+package org.example;
+
+/*import org.junit.jupiter.api.Test;*/
+
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+/*
 class CalculadoraTest {
-  
+
+    @org.junit.jupiter.api.Test
+    void sumar() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void restar() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void multiplicar() {
+    }
+}
+*/
+class CalculadoraTest {
+    @org.junit.jupiter.api.Test
+    public void testSuma(){
+        Calculadora cal=new Calculadora();
+        assertEquals(9,cal.sumar(6,3));
+    }
+/*
+    @Test
+    public void testSuma(){
+        Calculadora cal=new Calculadora();
+        assertEquals(9,cal.sumar(5,3));
+    }
+}*/
+
+    @org.junit.jupiter.api.Test
+    public void testResta(){
+        Calculadora cal=new Calculadora();
+        assertEquals(3,cal.restar(6,3));
+    }
+    @org.junit.jupiter.api.Test
+    public void testMultiplicar(){
+        Calculadora cal=new Calculadora();
+        assertEquals(21,cal.multiplicar(7,3));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testDividir(){
+        Calculadora cal=new Calculadora();
+        assertEquals(7,cal.dividir(21,3));
+    }
 }
